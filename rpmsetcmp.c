70207f20248780b7f4db6dd00d7efbeded21eb4b
Alexey Tourbin <alexey.tourbin@gmail.com>
2017-02-04 01:40:16 +0300

    rpmsetcmp.c: some work on the setcmp loop

--- rpmsetcmp.c
+++ rpmsetcmp.c
@@ -1,7 +1,9 @@
+#include <string.h>
+#include <stdbool.h>
 /*
  * To compare two decoded sets, we basically need to compare two arrays
  * of sorted numbers, v1[] and v2[].  This can be done with a merge-like
- * algorithm, which advances either v1 or v2 at each step (or both, when
+ * routine, which advances either v1 or v2 at each step (or both, when
  * two elements match).  We do something like this, but with a few twists.
  *
  * Note that, when comparing Requires against Provides, the Requires set
@@ -20,26 +22,124 @@
  * V_MAX sentinel at v1end.
  *
  * Moreover, when the Requires set is very sparse, it makes sense to step
- * a few elements at a time, and then step back a little bit using bisecting
- * (cf. Binary merging in [Knuth, Vol.3, p.203]).  This requires more than
- * one sentinel.
+ * a few elements at a time, and then somehow to step a little bit back.
+ * This will require more than one sentinel.
  */
-static int setcmp(const unsigned *v1, int n1, const unsigned *v2, int n2)
+static int setcmp(const unsigned *v1, size_t n1,
+		  const unsigned *v2, size_t n2)
 {
     /* Assume that the sets are equal.
      * These flags are cleared as the comparison progresses. */
-    int ge = 1;
-    int le = 1;
+    bool le = 1, ge = 1;
+    /* The general structure of a set-comparison loop is as follows. */
+#if defined(COMMENT) && !defined(COMMENT)
+    while (1) {
+	/* The "IFLT" part: */
+	if (*v1 < *v2) {
+	    /* There is an element in v1 which is not in v2.
+	     * Therefore, v1 is not "less or equal" than v2. */
+	    le = 0;
+	    /* Advance v1 as much as possible, using a specialized loop.
+	     * Sometimes, there will be a lot of elements to skip. */
+	}
+	/* The "IFGE" part: */
+	if (*v1 == *v2) {
+	    /* Advance v1 and v2, once; this is the second most probable
+	     * outcome ("else", we probably have an unmet dependency). */
+	}
+	else {
+	    /* There is an element in v2 which is not in v1.
+	     * Therefore, v1 is not "greater or equal" than v2. */
+	    ge = 0;
+	    /* Advance v2, just once.  After this is done, the next
+	     * comparison will probably yield (*v1 < *v2), which is
+	     * indeed the very first branch in the loop. */
+	}
+    }
+#endif
+    /* All the boundary checking is done inside the loop (this is because
+     * e.g. in the IFLT part, only the v1 boundary needs to be checked).
+     * To facilitate the checking even further, we preload the boundaries
+     * into registers. */
     const unsigned *v1end = v1 + n1;
     const unsigned *v2end = v2 + n2;
+    /* Since v2 gets advanced far less often than v1, its value is also
+     * preloaded into a register.  Whether v1 value should be preloaded
+     * in the same manner, the results of profiling are inconclusive. */
     unsigned v2val = *v2;
-    /* loop pieces */
-#define IFLT4			\
+    /* The comparison loop template. */
+#define CMPLOOP(N, ADV)		\
+    do {			\
+	while (1) {		\
+	    IFLT ## N(ADV);	\
+	    IFGE;		\
+	}			\
+    } while (0)
+    /* To advance v1, we provide two loops.  When the expected number
+     * of iterations is very small, the "tight" loop should be used.
+     * Otherwise, the "unrolled" loop can be beneficial.  Both loops
+     * can advance v1 speculatively by more than 1 (which implies
+     * that after the loop, v1 must somehow backtrack). */
+#define ADVANCE_V1_TIGHT(N)	\
+    while (*v1 < v2val)		\
+	v1 += N;
+#define ADVANCE_V1_UNROLLED(N)	\
+    while (1) {			\
+	/* Cf. Quicker sequential search in [Knuth, Vol.3, p.398] */ \
+	/* Four iterations work best here. */		\
+	if (v1[0*N] >= v2val) break;			\
+	if (v1[1*N] >= v2val) { v1 += 1*N; break; }	\
+	if (v1[2*N] >= v2val) { v1 += 2*N; break; }	\
+	if (v1[3*N] >= v2val) { v1 += 3*N; break; }	\
+	v1 += 4*N;		\
+    }
+    /* We're now able to provide a reference implementation for IFLT
+     * and IFGE, thus completing the loop. */
+#define IFLT1(ADV)		\
+    if (*v1 < v2val) {		\
+	le = 0;			\
+	v1++;			\
+	ADVANCE_V1_ ## ADV(1);	\
+	/* We don't use "do { STMT } while (0)" hygienic macros,
+	 * because we need to break out of the enclosing loop. */ \
+	if (v1 == v1end)	\
+	    break;		\
+    }
+#define IFGE			\
+    if (*v1 == v2val) {		\
+	v1++, v2++;		\
+	if (v1 == v1end)	\
+	    break;		\
+	if (v2 == v2end)	\
+	    break;		\
+	v2val = *v2;		\
+    }				\
+    else {			\
+	ge = 0;			\
+	v2++;			\
+	if (v2 == v2end)	\
+	    break;		\
+	v2val = *v2;		\
+    }
+    /* Here come additional/specialized IFLT parts which advance v1
+     * speculatively, after which they backtrack v1 using bisecting.
+     * Cf. Binary merging in [Knuth, Vol.3, p.203] */
+#define IFLT2(ADV)		\
+    if (*v1 < v2val) {		\
+	le = 0;			\
+	v1 += 2;		\
+	ADVANCE_V1_ ## ADV(2);	\
+	v1--;			\
+	if (*v1 < v2val)	\
+	    v1++;		\
+	if (v1 == v1end)	\
+	    break;		\
+    }
+#define IFLT4(ADV)		\
     if (*v1 < v2val) {		\
 	le = 0;			\
 	v1 += 4;		\
-	while (*v1 < v2val)	\
-	    v1 += 4;		\
+	ADVANCE_V1_ ## ADV(4);	\
 	v1 -= 2;		\
 	if (*v1 < v2val)	\
 	    v1++;		\
@@ -50,12 +150,11 @@ static int setcmp(const unsigned *v1, int n1, const unsigned *v2, int n2)
 	if (v1 == v1end)	\
 	    break;		\
     }
-#define IFLT8			\
+#define IFLT8(ADV)		\
     if (*v1 < v2val) {		\
 	le = 0;			\
 	v1 += 8;		\
-	while (*v1 < v2val)	\
-	    v1 += 8;		\
+	ADVANCE_V1_ ## ADV(8);	\
 	v1 -= 4;		\
 	if (*v1 < v2val)	\
 	    v1 += 2;		\
@@ -70,39 +169,16 @@ static int setcmp(const unsigned *v1, int n1, const unsigned *v2, int n2)
 	if (v1 == v1end)	\
 	    break;		\
     }
-#define IFGE			\
-    if (*v1 == v2val) {		\
-	v1++;			\
-	v2++;			\
-	if (v1 == v1end)	\
-	    break;		\
-	if (v2 == v2end)	\
-	    break;		\
-	v2val = *v2;		\
-    }				\
-    else {			\
-	ge = 0;			\
-	v2++;			\
-	if (v2 == v2end)	\
-	    break;		\
-	v2val = *v2;		\
-    }
     /* Choose the right stepper.  We can safely multiply by 16 here, see
      * a comment on the maximum set-string size in rpmss.c:encodeInit().
      * The constant is derived empirically. */
-    if (n1 >= 16 * n2) {
-	while (1) {
-	    IFLT8;
-	    IFGE;
-	}
-    }
-    else {
-	while (1) {
-	    IFLT4;
-	    IFGE;
-	}
-    }
-    /* return */
+    if (n1 >= 16 * n2)
+	CMPLOOP(8, UNROLLED);
+    else if (n1 >= 8 * n2)
+	CMPLOOP(4, TIGHT);
+    else
+	CMPLOOP(1, TIGHT);
+    /* If there are any elements left, this affects the result. */
     if (v1 < v1end)
 	le = 0;
     if (v2 < v2end)
@@ -115,9 +191,6 @@ static int setcmp(const unsigned *v1, int n1, const unsigned *v2, int n2)
 	return -1;
     return -2;
 }
-
-/* need memset */
-#include <string.h>
 
 /* The above technique requires sentinels properly installed
  * at the end of every Provides set. */
